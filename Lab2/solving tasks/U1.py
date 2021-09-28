8143f47ee043db2258fcff6dc5ed47a90db542c6
Дарья Денисовна Евстратова <alis131140@gmail.com>
2021-09-28 10:25:19 +0300

    new task2

--- Lab2/solving tasks/U1.py
+++ Lab2/solving tasks/U1.py
@@ -2,10 +2,11 @@ a = int(input())
 b = int(input())
 c = int(input())
 d = int(input())
-x = -b // a
-if (c * x) + d == 0:
+if a == 0 or b * c == a * d:
 	print('NO')
 elif a == 0 and b == 0:
 	print('INF')
+elif b % a == 0:
+	print(-b // a)
 else:
-	print(x)
+	print('NO')
