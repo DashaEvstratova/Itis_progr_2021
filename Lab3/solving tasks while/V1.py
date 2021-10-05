5bef219c0e12aab0f91d2f7f625221ed942d3207
Дарья Денисовна Евстратова <alis131140@gmail.com>
2021-10-05 11:43:37 +0300

    3 task

--- Lab3/solving tasks while/V1.py
+++ Lab3/solving tasks while/V1.py
@@ -1,34 +1,25 @@
+a = 0
+b = 0
+c = 0
 n = int(input())
-c1 = 1
-c2 = 1
-s2 = 1
-s1 = 1
+q = []
 while n != 0:
-    n1 = int(input())
-    if n > n1:
-        c1 += 1
-        if c2 > s2:
-            s2 = c2
-        c2 = 1
-    elif n < n1:
-        if c1 > s1:
-            s1 = c1
-        c2 += 1
-        c1 = 1
+    q.append(n)
+    n = int(input())
+for i in range(0, len(q)-1):
+    if q[i] > q[i+1]:
+        c += 1
     else:
-        if c2 > s2:
-            s2 = c2
-        c2 = 1
-        if c1 > s1:
-            s1 = c1
-        c2 += 1
-        c1 = 1
-    n = n1
-if c1 > s1:
-    s1 = c1
-if c2 > s2:
-    s2 = c2
-if s1 > s2:
-    print(s1)
-else:
-    print(s2)
+        a = max(a, c)
+        c = 0
+a = max(a, c)
+c = 0
+for i in range(0, len(q)-1):
+    if q[i] < q[i+1]:
+        c += 1
+    else:
+        b = max(a, c)
+        c = 0
+b = max(a, c)
+ 
+print(max(a, b))
