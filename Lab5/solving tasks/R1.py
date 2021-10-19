aaf943d06b33a1a7dbd1fb3e24e9c995e349da75
Дарья Денисовна Евстратова <alis131140@gmail.com>
2021-10-19 10:56:08 +0300

    5 tasks

--- Lab5/solving tasks/R1.py
+++ Lab5/solving tasks/R1.py
@@ -5,29 +5,37 @@ d = float(input())
 e = float(input())
 f = float(input())
 if a == 0 and b == 0 and c == 0 and d == 0 and e == 0 and f == 0:
-    print(5)
-elif (a == 0 and b == 0 and e != 0) or (c == 0 and d == 0 and f != 0) or (a * d == b * c and a * f != c * e):
-    print(0)
+    print('5')
+elif a * d == b * c and a * f != c * e:
+    print('0')
+elif a == 0 and b == 0 and e != 0:
+    print('0')
+elif c == 0 and d == 0 and f != 0:
+    print('0')
+elif a == 0 and c == 0 and b * f != d * e:
+    print('0')
+elif b == 0 and d == 0 and a * f != c * e:
+    print('0')
 elif a * d == b * c and a * f == c * e:
     if b == 0 and d == 0:
         if a != 0 and c != 0:
-            print(3, e / a)
+            print('3', e / a)
         elif a == 0:
             if e == 0:
-                print(3, f / c)
+                print('3', f / c)
         elif c == 0:
             if f == 0:
-                print(3, e / a)
+                print('3', e / a)
     elif a == 0 and c == 0:
         if b != 0:
-            print(4, e / b)
+            print('4', e / b)
         elif d != 0:
-            print(4, f / d)
+            print('4', f / d)
     elif b != 0:
-        print(1, -a / b, e / b)
+        print('1', -a / b, e / b)
     elif d != 0:
-        print(1, -c / d, f / d)
+        print('1', -c / d, f / d)
 else:
     x = (e * d - b * f) / (a * d - b * c)
     y = (a * f - e * c) / (a * d - b * c)
-    print(2, x, y)
+    print('2', x, y)
